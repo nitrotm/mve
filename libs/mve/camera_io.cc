c49de7ce410f258e2ef43550e0d3d8bf4c43eeda
Samir Aroudj <samir.aroudj@gris.informatik.tu-darmstadt.de>
2018-02-12 21:39:08 +0100

    output of new Cameras.txt

--- libs/mve/camera_io.cc
+++ libs/mve/camera_io.cc
@@ -36,9 +36,10 @@ save_camera_infos (const std::vector<View::Ptr> &views, std::string const& file_
 		if (nullptr != views[view_idx])
 			++actual_view_count;
 
-	out << "MVE camera infos 1.0\n";
+	out << "MVE camera infos 1.1\n";
 	out << "camera_count = " << actual_view_count << "\n";
-
+	out << "\n";
+	
 	/* Write all cameras infos to file. */
 	for (size_t view_idx = 0; view_idx < view_count; ++view_idx)
 	{
@@ -50,23 +51,24 @@ save_camera_infos (const std::vector<View::Ptr> &views, std::string const& file_
 		std::string name = view.get_name();
 		if (name.empty())
 			name = std::to_string(view.get_id());
-		
-		/* identifiers */
-		out << "id = " << view.get_id() << "\n";
-		out << "name = " << name << "\n";
 
 		/* intrinsics */
 		out << "focal_length = " << cam.flen << "\n";
-		out << "principle_point = " << cam.ppoint[0] << " " << cam.ppoint[1] << "\n";
-		out << "pixel_aspect_ratio = " << cam.paspect << "\n";
+		out << "pixel_aspect = " << cam.paspect << "\n";
+		out << "principal_point = " << cam.ppoint[0] << " " << cam.ppoint[1] << "\n";
 		out << "camera_distortion = " << cam.dist[0] << " " << cam.dist[1] << "\n";
 
 		/* extrinsics */
-		out << "translation = " << cam.trans[0] << " " << cam.trans[1] << " " << cam.trans[2] << "\n";
 		out << "rotation = ";
 		out	<< cam.rot[0] << " " << cam.rot[1] << " " << cam.rot[2] << " ";
 		out	<< cam.rot[3] << " " << cam.rot[4] << " " << cam.rot[5] << " ";
 		out	<< cam.rot[6] << " " << cam.rot[7] << " " << cam.rot[8] << "\n";
+		out << "translation = " << cam.trans[0] << " " << cam.trans[1] << " " << cam.trans[2] << "\n";
+		
+		/* identifiers */
+		out << "id = " << view.get_id() << "\n";
+		out << "name = " << name << "\n";
+		out << "\n";
 	}
 }
 
